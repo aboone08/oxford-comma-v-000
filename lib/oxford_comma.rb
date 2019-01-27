e40fe8b0beded95acd67a68814a4fee6d68c346e
Andranique Boone <andranique.boone@gmail.com>
2019-01-27 18:56:30 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,6 @@ def oxford_comma(array)
   array.each_with_index do |element, index|
     puts "the element is #{element}"
     puts "the index is #{index}"
-  end
   if index>1
   array.insert(-2, "and").to_s
   end
