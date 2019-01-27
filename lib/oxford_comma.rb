462aa0e0636d233ad0a062ecbfbb7fcf94ebdc2e
Andranique Boone <andranique.boone@gmail.com>
2019-01-27 19:19:51 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,6 +5,6 @@ def oxford_comma(array)
   when 2
     return array[0]+" and "+array[1] if array.size==2
   else
-    array.join(",").insert(-1,"and")
+    array.join(",").insert(-1,"and") if array.size>=3
   end
 end
