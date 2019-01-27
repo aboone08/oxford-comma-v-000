bb85895218bacfa58428177ee99d2a2892a99908
Andranique Boone <andranique.boone@gmail.com>
2019-01-27 18:48:59 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   array = [""]
   array.each_with_index do |element, index|
-    return element 
+    return element
     return index
   end
   array.insert(-2, "and").to_s
