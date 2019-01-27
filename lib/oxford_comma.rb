81a40d0e138e4f7788371890b914a529136bc4d2
Andranique Boone <andranique.boone@gmail.com>
2019-01-27 19:15:34 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,6 +5,6 @@ def oxford_comma(array)
   when 2
     return array[0]+" and "+array[1] if array.size==2
   else
-    return array.to_s.insert(-2, "and") if array.size >2
+    return array[0].to_s.insert(-2, "and") if array.size >2
   end
 end
