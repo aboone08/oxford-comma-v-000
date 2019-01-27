de370020febaf9855217fa3bc0070278cace1ad0
Andranique Boone <andranique.boone@gmail.com>
2019-01-27 18:58:22 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,9 @@
 def oxford_comma(array)
   array = [""]
   array.each_with_index do |element, index|
-
-  if index>1
     puts "the element is #{element}"
     puts "the index is #{index}"
-    array.insert(-2, "and").to_s
+  if index>1
+  array.insert(-2, "and").to_s
+end
 end
