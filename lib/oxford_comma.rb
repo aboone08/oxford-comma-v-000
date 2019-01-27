ad169ad8393a6ea978f1e603071e1c65a819712c
Andranique Boone <andranique.boone@gmail.com>
2019-01-27 20:56:16 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,6 +5,6 @@ def oxford_comma(array)
   when 2
     return array[0]+" and "+array[1] if array.size==2
   else
-    array.join(", ").tap{|s| s[s.rindex(', '), 2]='and'} if array.size>2
+    array.join(", ").tap{|s| s[s.rindex(', '), 2]=', and'} if array.size>2
   end
 end
