8aefd96b544b1ef9d8f1dd3e08972fcf052126ea
Andranique Boone <andranique.boone@gmail.com>
2019-01-27 20:53:00 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,6 +5,6 @@ def oxford_comma(array)
   when 2
     return array[0]+" and "+array[1] if array.size==2
   else
-    array.reverse_each.join(", ").sub(",", "and ,")if array.size>2
+    array.reverse.join(", ").sub(",", "and ,")if array.size>2
   end
 end
